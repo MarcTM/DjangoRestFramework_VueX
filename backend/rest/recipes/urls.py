7ea768a7b1bbe530b472c2c312a27e84041f7c6a
MarcTM <marctorresmartinez@gmail.com>
2021-01-26 15:37:10 +0100

    Viewsets

--- backend/rest/recipes/urls.py
+++ backend/rest/recipes/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import GenericAPIView, DetailsAPIView
+from django.urls import path, include
+from .views import GenericAPIView, DetailsAPIView, RecipeViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
+
     path('recipes/', GenericAPIView.as_view()),
     path('recipes/<int:id>', DetailsAPIView.as_view())
 ]
--- backend/rest/recipes/views.py
+++ backend/rest/recipes/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 from .serializers import RecipeSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics, mixins
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+
+
+# class RecipeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
+#                     mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+
+#     serializer_class = RecipeSerializer
+#     queryset = Recipe.objects.all()
+
+
+
+# class RecipeViewSet(viewsets.ViewSet):
+
+#     def list(self, request):
+#         recipes = Recipe.objects.all()
+#         serializer = RecipeSerializer(recipes, many=True)
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = RecipeSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+#     def retrieve(self, request, pk=None):
+#         queryset = Recipe.objects.all()
+#         recipe = get_object_or_404(queryset, pk=pk)
+#         serializer = RecipeSerializer(recipe)
+#         return Response(serializer.data)
+
+#     def update(self, request, pk=None):
+#         recipe = Recipe.objects.get(pk=pk)
+#         serializer = RecipeSerializer(recipe, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 # Get, create recipes
@@ -13,6 +63,9 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
 
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
 
 
     def get(self, request, id=None):
