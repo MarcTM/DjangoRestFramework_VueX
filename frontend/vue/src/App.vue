70c2dccea50bfe98f37a4023f700cb5ab5c3be01
MarcTM <marctorresmartinez@gmail.com>
2021-01-28 20:15:21 +0100

    Apaños

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <Header />
+
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  <router-view />
 </template>
+
+<script>
+  import Header from '@/views/layout/Header.vue';
+
+  export default {
+    name: "App",
+    components: {
+      Header
+    }
+  }
+</script>
 
 <style>
 #app {
---
+++ frontend/vue/src/views/layout/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header">
+  <h1>HEADER</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Header extends Vue {}
+</script>
