5682ae5af3e3f50efe4534548d0a4f2de9d9faad
MarcTM <marctorresmartinez@gmail.com>
2021-02-15 20:30:59 +0100

    checkpoint

--- backend/rest/account/admin.py
+++ backend/rest/account/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from account.models import Account
 
-
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'username', 'created_at', 'last_login', 'is_admin', 'is_staff')
     search_fields = ('email', 'username')
--- backend/rest/meals/views.py
+++ backend/rest/meals/views.py
@@ -10,37 +10,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 
 
-# class MealViewSet(viewsets.ViewSet):
-
-#     def list(self, request):
-#         meals = Meal.objects.all()
-#         serializer = MealSerializer(meals, many=True)
-#         return Response(serializer.data)
-
-#     def create(self, request):
-#         serializer = MealSerializer(data=request.data)
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-#     def retrieve(self, request, pk=None):
-#         queryset = Meal.objects.all()
-#         meal = get_object_or_404(queryset, pk=pk)
-#         serializer = MealSerializer(meal)
-#         return Response(serializer.data)
-
-#     def update(self, request, pk=None):
-#         meal = Meal.objects.get(pk=pk)
-#         serializer = MealSerializer(meal, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 # Get, create meals
 class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
 
@@ -52,10 +21,7 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
 
 
     def get(self, request, id=None):
-        if id:
-            return self.retrieve(request)
-        else:
-            return self.list(request)
+        return self.list(request)
 
     def post(self, request):
         return self.create(request)
